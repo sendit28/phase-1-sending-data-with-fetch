c1e068d535c1dec356ef1249cb16c4a76af6bfd2
+ <+>
2022-07-31 12:55:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: userEmail,
+        }),
+    })
+      .then((response) => response.json())
+      .then((userObject) => { 
+        //add the id of the userObject to the DOM
+      document.body.append(userObject.id)
+      })
+      .catch((error) => { 
+        //error.message append message to DOM
+      document.body.append(error.message)
+      })
+  } 
+
+
